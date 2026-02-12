e29d758c6517b8a4fdec2b8db7c410e99770afcc
Devys <145410373+YuriTriches@users.noreply.github.com>
2026-02-12 17:02:52 -0300

    Delete site pra minha namorada q amo tanto directory
    erro meu

--- site pra minha namorada q amo tanto/menu.js
+++
@@ -1,7 +0,0 @@
-    const menuBtn = document.querySelector(".menu");
-    const socials = document.querySelector(".socials");
-
-    menuBtn.addEventListener("click", () => {
-        socials.classList.toggle("ativo");
-    });
-
