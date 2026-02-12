0b0497765de9e258c2d1f62f8aaa5723ef06c8fc
Devys <145410373+YuriTriches@users.noreply.github.com>
2026-02-12 17:02:17 -0300

    Add files via upload

---
+++ site pra minha namorada q amo tanto/menu.js
@@ -0,0 +1,7 @@
+    const menuBtn = document.querySelector(".menu");
+    const socials = document.querySelector(".socials");
+
+    menuBtn.addEventListener("click", () => {
+        socials.classList.toggle("ativo");
+    });
+
